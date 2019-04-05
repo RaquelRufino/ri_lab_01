03d3f1bd3e911224cdbaf96ca9bcdd57a276d424
Your Name <raquel.paz@ccc.ufcg.edu.br>
2019-04-05 10:40:48 -0300

    lab ri

--- ri_lab_01/settings.py
+++ ri_lab_01/settings.py
@@ -31,18 +31,18 @@ USER_AGENTS = [
 ]
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'ri_lab_01 (+http://www.yourdomain.com)'
+USER_AGENT = 'ri_lab_01_raquel_paz'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
-# Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+#Configure maximum concurrent requests performed by Scrapy (default: 16)
+CONCURRENT_REQUESTS = 8
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 5
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -75,11 +75,13 @@ DOWNLOADER_MIDDLEWARES = {
 }
 
 # Define used proxies
+
 ROTATING_PROXY_LIST = [
-  '189.51.153.195:57898', '45.162.37.79:8080',   '179.108.187.9:8080',
-  '177.23.104.38:61536',  '187.32.123.177:3128', '45.71.80.34:37122',
-  '45.162.37.79:8080',    '186.250.55.233:50741','179.108.187.9:8080',
-  '187.32.123.177:3128',  '187.32.123.177:3128', '177.72.92.24:23500'
+
+'189.51.153.195:57898', '45.162.37.79:8080',   '179.108.187.9:8080',
+'177.23.104.38:61536',  '187.32.123.177:3128', '45.71.80.34:37122',
+'45.162.37.79:8080',    '186.250.55.233:50741','179.108.187.9:8080',
+'187.32.123.177:3128',  '187.32.123.177:3128', '177.72.92.24:23500' 
 ]
 
 # Enable or disable downloader middlewares
@@ -104,7 +106,7 @@ ITEM_PIPELINES = {
 }
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = True
 # The initial download delay
 #AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
--- ri_lab_01/spiders/oantagonista.py
+++ ri_lab_01/spiders/oantagonista.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import json
-
+import datetime
 from ri_lab_01.items import RiLab01Item
 from ri_lab_01.items import RiLab01CommentItem
 
@@ -10,6 +10,8 @@ class OantagonistaSpider(scrapy.Spider):
     name = 'oantagonista'
     allowed_domains = ['oantagonista.com']
     start_urls = []
+    n_page = 1
+    limitDate = datetime.datetime(2018,1,1)
 
     def __init__(self, *a, **kw):
         super(OantagonistaSpider, self).__init__(*a, **kw)
@@ -18,14 +20,67 @@ class OantagonistaSpider(scrapy.Spider):
         self.start_urls = list(data.values())
 
     def parse(self, response):
-        #
-        # inclua seu código aqui
-        #
+        
+
+        def checkDateLimit(response, lowerLimit):
+            currentDate = response.css('time.entry-date ::attr(datetime)').get()
+            currentDate = currentDate.split(' ')[0].split('-')
+
+            date =  datetime.datetime(int(currentDate[-3]), int(currentDate[-2]), int(currentDate[-1]))
+
+            return date > lowerLimit
+
+
+        if not checkDateLimit(response, self.limitDate) :
+            return
+
+
+        articles = response.css('article')
+
+        for href in articles.css('a.article_link::attr(href)'):
+            yield response.follow(href, callback= self.scrapeNewsPage)
+
         page = response.url.split("/")[-2]
+
+        nextPage = self.start_urls[0] + 'pagina/' + str(self.n_page) +'/?ajax'
+        self.n_page = self.n_page + 1
+
         filename = 'quotes-%s.html' % page
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
-        #
-        #
-        #
+
+        
+        yield scrapy.Request(nextPage, callback=self.parse)
+
+
+    def scrapeNewsPage(self, response):
+
+        encodeUTF8 = lambda target : target.encode('UTF-8')
+
+
+        articleAuthor = response.css('header.entry-header div::text').get().replace('Por ','')
+
+
+        articleText = ""
+
+        for paragraph in response.css('div.entry-content p::text'):
+            articleText = articleText + paragraph.get()
+        
+        reverseDate = lambda date : '/'.join(date.split('-')[::-1])
+
+        date = response.css('time.entry-date ::attr(datetime)').get()
+        year,time = date.split(' ')
+        year = reverseDate(year)
+
+        date =  year + " " + time
+
+        yield {
+            'title': encodeUTF8(response.css('h1.entry-title::text').get()),
+            'subtitle': 'Nao tem subtitle',
+            'author': encodeUTF8(articleAuthor) if articleAuthor.strip() else "Nao tem autor",
+            'date': encodeUTF8(date),
+            'section': encodeUTF8(response.css('header.entry-header span.postmeta span.categoria a::text').get()),
+            'text': encodeUTF8(articleText),
+            'url': response.request.url
+        }
